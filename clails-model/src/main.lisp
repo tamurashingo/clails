d2054df191e61867f5f18a399715717e907b9b99
tamura shingo <tamura.shingo@gmail.com>
2024-11-05 15:11:53 +0900

    rm main branch

--- clails-model/src/main.lisp
+++
@@ -1,9 +0,0 @@
-(in-package #:cl-user)
-(defpackage #:clails-model/main
-  (:nicknames #:clails-model)
-  (:use #:cl)
-  (:import-from #:clails-model/base-model
-                #:<base-model>)
-  (:export #:<base-model>))
-(in-package #:clails-model/main)
-
