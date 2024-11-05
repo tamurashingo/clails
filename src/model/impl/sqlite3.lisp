10aedac13ef8ea663355d3809fc4743d4aa1a9f1
tamura shingo <tamura.shingo@gmail.com>
2024-11-05 13:12:45 +0900

    add sqlite3 test

--- src/model/impl/sqlite3.lisp
+++ src/model/impl/sqlite3.lisp
@@ -2,10 +2,15 @@
 (defpackage #:clails/model/impl/sqlite3
   (:use #:cl)
   (:import-from #:clails/environment
-                #:<database-type-sqlite3>)
+                #:<database-type-sqlite3>
+                #:*database-config*)
   (:import-from #:clails/model/migration
                 #:create-column-impl
-                #:parse-add-column-impl))
+                #:apply-create-database-impl
+                #:apply-create-migration-table-impl
+                #:parse-add-column-impl)
+  (:import-from #:clails/model/connection
+                #:get-connection-direct-impl))
 (in-package #:clails/model/impl/sqlite3)
 
 (defparameter *sqlite3-type-convert*
@@ -28,7 +33,24 @@
           (type-convert type)
           (if not-null-p "NOT NULL" "NULL")
           (if primary-key-p "PRIMARY KEY" "")
-          (if auto-increment-p "AUTO_INCREMENT" "")))
+          (if auto-increment-p "AUTOINCREMENT" "")))
+
+(defmethod get-connection-direct-impl ((database-type <database-type-sqlite3>) &key no-database)
+  (let ((database-name (getf *database-config* :database)))
+    (dbi:connect :sqlite3
+                 :database-name database-name)))
+
+(defparameter CREATE-MIGRATION-TABLE
+  (format NIL "CREATE TABLE migration (~
+                  migration_name varchar(255) NOT NULL PRIMARY KEY, ~
+                  created_at dateteime NOT NULL DEFAULT CURRENT_TIMESTAMP)~
+              "))
+
+(defmethod apply-create-database-impl ((database-type <database-type-sqlite3>) connection)
+  'NOP)
+
+(defmethod apply-create-migration-table-impl ((database-type <database-type-sqlite3>) connection)
+  (dbi:do-sql connection CREATE-MIGRATION-TABLE))
 
 (defmethod parse-add-column-impl ((database-type <database-type-sqlite3>) columns)
   (format NIL "add column ~{ ~A ~^, ~}" columns))
--- test/model/impl/sqlite3.lisp
+++ test/model/impl/sqlite3.lisp
@@ -7,6 +7,7 @@
 
 (setup
    (setf clails/environment:*database-type* (make-instance 'clails/environment::<database-type-sqlite3>))
+   (setf clails/environment:*database-config* '(:database "/app/volumes/clails_test.sqlite3"))
 )
 
 (defhook initialize-tbl :before
@@ -20,7 +21,7 @@
            (result-raw (clails/model/migration::parse-migration s))
            (result (ppcre:regex-replace-all "\\s+" result-raw " "))
            (expect (ppcre:regex-replace-all "\\s+" (format nil "create table todo ( ~
-                      id integer NOT NULL PRIMARY KEY AUTO_INCREMENT , ~
+                      id integer NOT NULL PRIMARY KEY AUTOINCREMENT , ~
                       created_at datetime NOT NULL , ~
                       updated_at datetime NOT NULL , ~
                       title varchar(255) NULL , ~
@@ -45,4 +46,50 @@
                             ("done" :TYPE :BOOLEAN)
                             ("done-at" :TYPE :DATETIME))))
                clails/model/migration::*tbl*))))
+
+(deftest create-database
+  (clails/model/migration::%db/create)
+  (clails/model/connection::with-db-connection-direct (connection)
+    ;; check migration table exists
+    (let* ((query (dbi:prepare connection "select tbl_name from sqlite_master where tbl_name = ?"))
+           (result (dbi:execute query (list "migration"))))
+      (ok (string= "migration" (getf (dbi:fetch result) :|tbl_name|))))))
+
+(deftest migration
+  (clails/model/migration::%db/migrate "/app/test/")
+  (clails/model/connection::with-db-connection-direct (connection)
+    ;; check schema
+    (let* ((result (dbi:fetch-all (dbi:execute (dbi:prepare connection "pragma table_info('todo')")
+                                               '())))
+           (id (first result))
+           (created-at (second result))
+           (updated-at (third result))
+           (title (fourth result))
+           (done (fifth result))
+           (done-at (sixth result)))
+      ;; id
+      (ok (string= "id" (getf id :|name|)))
+      (ok (string= "INTEGER" (getf id :|type|)))
+      (ok (equal 1 (getf id :|notnull|)))
+      (ok (equal 1 (getf id :|pk|)))
+      ;; created_at
+      (ok (string= "created_at" (getf created-at :|name|)))
+      (ok (string= "datetime" (getf created-at :|type|)))
+      (ok (equal 1 (getf created-at :|notnull|)))
+      ;; updated_at
+      (ok (string= "updated_at" (getf updated-at :|name|)))
+      (ok (string= "datetime" (getf updated-at :|type|)))
+      (ok (equal 1 (getf updated-at :|notnull|)))
+      ;; title
+      (ok (string= "title" (getf title :|name|)))
+      (ok (string= "varchar(255)" (getf title :|type|)))
+      (ok (equal 0 (getf title :|notnull|)))
+      ;; done
+      (ok (string= "done" (getf done :|name|)))
+      (ok (string= "boolean" (getf done :|type|)))
+      (ok (equal 0 (getf done :|notnull|)))
+      ;; done_at
+      (ok (string= "done_at" (getf done-at :|name|)))
+      (ok (string= "datetime" (getf done-at :|type|)))
+      (ok (equal 0 (getf done-at :|notnull|))))))
 
