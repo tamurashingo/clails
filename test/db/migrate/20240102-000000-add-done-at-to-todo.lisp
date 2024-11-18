9fb2ae107e6bc893fa0ea5053cb3f5cbd425130e
tamura shingo <tamura.shingo@gmail.com>
2024-11-18 18:58:21 +0900

    mod migration format

--- test/db/migrate/20240102-000000-add-done-at-to-todo.lisp
+++ test/db/migrate/20240102-000000-add-done-at-to-todo.lisp
@@ -1,2 +1,11 @@
-(add-column "todo"
-  (("done-at" :type :datetime)))
+(in-package #:clails-test/model/db)
+
+(defmigration "20240102-000000-add-done-at-to-todo"
+  (:up #'(lambda (conn)
+           (add-column conn :table "todo"
+                            :columns '(("done-at" :type :datetime
+                                                  :not-null nil))))
+  :down #'(lambda (conn)
+            (drop-column conn :table "todo"
+                              :column "done-at"))))
+
