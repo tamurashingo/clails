ff613bb349a124eeb7d10d91a3ad258c8dde13b4
tamura shingo <tamura.shingo@gmail.com>
2024-11-19 05:19:24 +0900

    mod migration

--- src/model/impl/mysql.lisp
+++ src/model/impl/mysql.lisp
@@ -5,12 +5,19 @@
                 #:<database-type-mysql>
                 #:*database-config*)
   (:import-from #:clails/model/migration
-                #:create-column-impl
-                #:apply-create-database-impl
-                #:apply-create-migration-table-impl
-                #:parse-add-column-impl)
+                #:create-table-impl
+                #:add-column-impl
+                #:add-index-impl
+                #:drop-table-impl
+                #:drop-column-impl
+                #:drop-index-impl
+                #:ensure-database-impl
+                #:ensure-migration-table-impl)
   (:import-from #:clails/model/connection
-                #:get-connection-direct-impl))
+                #:get-connection-direct-impl)
+  (:import-from #:clails/util
+                #:kebab->snake
+                #:mandatory-check))
 (in-package #:clails/model/impl/mysql)
 
 (defparameter *mysql-type-convert*
@@ -23,17 +30,104 @@
     (:date . "date")
     (:time . "time")
     (:boolean . "boolean")))
-  
+
 (defun type-convert (type)
   (cdr (assoc type *mysql-type-convert*)))
 
-(defmethod create-column-impl ((database-type <database-type-mysql>) &key column-name type not-null-p primary-key-p auto-increment-p)
-  (format NIL " ~A ~A ~A ~A ~A "
-          column-name
-          (type-convert type)
-          (if not-null-p "NOT NULL" "NULL")
-          (if primary-key-p "PRIMARY KEY" "")
-          (if auto-increment-p "AUTO_INCREMENT" "")))
+
+(defmethod create-table-impl ((database-type <database-type-mysql>) connection &key table columns constraints)
+  (mandatory-check table columns)
+  (let ((query (gen-create-table table (append '(("id" :type :integer
+                                                       :not-null T
+                                                       :primary-key T
+                                                       :auto-increment T)
+                                                 ("created-at" :type :datetime
+                                                               :not-null T)
+                                                 ("updated-at" :type :datetime
+                                                               :not-null T))
+                                               columns))))
+    (dbi:do-sql connection query)))
+
+(defmethod add-column-impl ((database-type <database-type-mysql>) connection &key table columns)
+  (mandatory-check table columns)
+  (let ((query (gen-add-column table columns)))
+    (dbi:do-sql connection query)))
+  
+(defmethod add-index-impl ((database-type <database-type-mysql>) connection &key table index columns)
+  (mandatory-check table index columns)
+  (let ((query (gen-add-index table index columns)))
+    (dbi:do-sql connection query)))
+
+(defmethod drop-table-impl ((database-type <database-type-mysql>) connection &key table)
+  (mandatory-check table)
+  (let ((query (gen-drop-table table)))
+    (dbi:do-sql connection query)))
+
+(defmethod drop-column-impl ((database-type <database-type-mysql>) connection &key table column)
+  (mandatory-check table column)
+  (let ((query (gen-drop-column table column)))
+    (dbi:do-sql connection query)))
+
+(defmethod drop-index-impl ((database-type <database-type-mysql>) connection &key table index)
+  (mandatory-check table index)
+  (let ((query (gen-drop-index table index)))
+    (dbi:do-sql connection query)))
+
+
+(defun gen-create-table (table columns)
+  (format NIL "CREATE TABLE ~A (~{~A~^, ~})" (kebab->snake table)
+              (loop for col in columns
+                    collect (parse-column col))))
+
+(defun gen-add-column (table columns)
+  (format NIL "ALTER TABLE ~A ~{ ADD COLUMN ~A ~^, ~}"
+              table
+              (loop for col in columns
+                    collect (parse-column col))))
+
+(defun gen-drop-table (table)
+  (format NIL "DROP TABLE ~A" (kebab->snake table)))
+
+(defun gen-add-index (table index columns)
+  (format NIL "CREATE INDEX ~A ON ~A (~{~A~^, ~})"
+              (kebab->snake index)
+              (kebab->snake table)
+              (loop for col in columns
+                    collect (kebab->snake col))))
+
+(defun gen-drop-column (table columns)
+  (format NIL "ALTER TABLE ~A ~{ DROP COLUMN ~A ~^, ~}"
+              (kebab->snake table)
+              (loop for col in columns
+                    collect (kebab->snake col))))
+
+(defun gen-drop-index (table index)
+  (format NIL "DROP INDEX ~A ON ~A"
+              (kebab->snake index)
+              (kebab->snake table)))
+
+
+(defun parse-column (col)
+  (let* ((column-name (kebab->snake (first col)))
+         (attr (rest col))
+         (type (getf attr :type))
+         (not-null-p (getf attr :not-null))
+         (primary-key-p (getf attr :primary-key))
+         (auto-increment-p (getf attr :auto-increment))
+         (default-value (getf attr :default-value)))
+    ;(check-type-valid type)
+    (create-column column-name type not-null-p primary-key-p auto-increment-p default-value)))
+
+
+(defun create-column (column-name type not-null-p primary-key-p auto-increment-p default-value)
+  (format NIL " ~A ~A~@[ NOT NULL~*~]~@[ PRIMARY KEY~*~]~@[ AUTO_INCREMENT~*~]~@[ DEFAULT ~A~]"
+              column-name
+              (type-convert type)
+              not-null-p
+              primary-key-p
+              auto-increment-p
+              default-value))
+
 
 (defmethod get-connection-direct-impl ((database-type <database-type-mysql>) &key no-database)
   (let ((database-name (getf *database-config* :database))
@@ -56,21 +150,19 @@
 
 
 (defparameter CREATE-DATABASE
-  "CREATE DATABASE ~A CHARACTER SET utf8mb4 COLLATE utf8mb4_bin")
+  "CREATE DATABASE IF NOT EXISTS ~A CHARACTER SET utf8mb4 COLLATE utf8mb4_bin")
 
 (defparameter CREATE-MIGRATION-TABLE
-  (format NIL "CREATE TABLE migration (~
+  (format NIL "CREATE TABLE IF NOT EXISTS migration (~
                  migration_name varchar(255) NOT NULL PRIMARY KEY, ~
                  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP)~
               "))
 
 
-(defmethod apply-create-database-impl ((database-type <database-type-mysql>) connection)
+(defmethod ensure-database-impl ((database-type <database-type-mysql>) connection)
   (let ((database-name (getf *database-config* :database)))
     (dbi:do-sql connection (format NIL CREATE-DATABASE database-name))))
 
-(defmethod apply-create-migration-table-impl ((database-type <database-type-mysql>) connection)
+(defmethod ensure-migration-table-impl ((database-type <database-type-mysql>) connection)
   (dbi:do-sql connection CREATE-MIGRATION-TABLE))
 
-(defmethod parse-add-column-impl ((database-type <database-type-mysql>) columns)
-  (format NIL "add column ~{ ~A ~^, ~}" columns))
--- src/model/migration.lisp
+++ src/model/migration.lisp
@@ -6,164 +6,169 @@
   (:import-from #:clails/util
                 #:kebab->snake)
   (:import-from #:clails/model/connection
-                #:with-db-connection-direct)
-  (:export #:migrate-all))
+                #:with-db-connection-direct
+                #:with-db-connection)
+  (:export #:defmigration
+           #:create-table
+           #:add-column
+           #:add-index
+           #:drop-table
+           #:drop-column
+           #:drop-index
+           #:create-table-impl
+           #:add-column-impl
+           #:add-index-impl
+           #:drop-table-impl
+           #:drop-column-impl
+           #:drop-index-impl))
 (in-package #:clails/model/migration)
 
-(defparameter *tbl* '())
+;;; migration list
+(defparameter *migrations* '())
 
+;;; Types used in migration
 (defparameter *type-list*
   '(:string :text :integer :float :decimal :datetime :date :time :boolean))
 
+;;;
+;;; Migration DSL
+;;; up: migration up function. Takes CL-DBI:DBI-CONNECTION as an argument
+;;; down: migration down function. Takes CL-DBI:DBI-CONNECTION as an argument
+;;;
+;;; usage:
+;;; (defmigration "create-table-todo"
+;;;   (:up #'(lambda (conn)
+;;;            (create-table conn :table "todo"
+;;;                               :columns '(("title" :type :string
+;;;                                                   :not-null T)
+;;;                                          ("done" :type :boolean
+;;;                                                  :default-value NIL)))
+;;;            (add-index conn :table "todo"
+;;;                            :index "idx-title"
+;;;                            :columns '("title")))
+;;;    :down #'(lambda (conn)
+;;;              (drop-table conn :table "todo"))))
+;;;
+(defmacro defmigration (migration-name body)
+  "Migration DSL"
+  `(uiop:appendf *migrations*
+                 (list (list :migration-name ,migration-name
+                             :up ,(getf body :up)
+                             :down ,(getf body :down)))))
 
 
-(defun %db/create ()
-  (with-db-connection-direct (connection :no-database T)
-    (apply-create-database connection))
-  (with-db-connection-direct (connection)
-    (apply-create-migration-table connection)))
+;;;;
+;;;; Functions dependent on the implementation of each Database
+;;;;
+
+(defgeneric create-table-impl (database-type conn &rest args &key &allow-other-keys)
+  (:documentation "Implementation of create table"))
+
+(defgeneric add-column-impl (database-type conn &rest args &key &allow-other-keys)
+  (:documentation "Implementation of add column"))
+
+(defgeneric add-index-impl (database-type conn &rest args &key &allow-other-keys)
+  (:documentation "Implementation of add index"))
+
+(defgeneric drop-table-impl (database-type conn &rest args &key &allow-other-keys)
+  (:documentation "Implementation of drop table"))
+
+(defgeneric drop-column-impl (database-type conn &rest args &key &allow-other-keys)
+  (:documentation "Implementation of drop column"))
+
+(defgeneric drop-index-impl (database-type conn &rest args &key &allow-other-keys)
+  (:documentation "Implementation of drop index"))
+
+(defgeneric ensure-database-impl (database-type connection)
+  (:documentation "Implementation of ensure database"))
+
+(defgeneric ensure-migration-table-impl (database-type connection)
+  (:documentation "Implementation of ensure migration table"))
 
 
-(defun apply-create-database (connection)
-  (apply-create-database-impl *database-type* connection))
+;;;;
+;;;; exported functions
+;;;;
 
-(defun apply-create-migration-table (connection)
-  (apply-create-migration-table-impl *database-type* connection))
+(defun create-table (conn &rest args &key &allow-other-keys)
+  "Run create table query according to the database implementation"
+  (apply #'create-table-impl *database-type* conn args))
 
-(defgeneric apply-create-database-impl (database-type connection)
-  (:documentation "Implementation of create database"))
+(defun add-column (conn &rest args &key &allow-other-keys)
+  "Run add column query according to the database implementation"
+  (apply #'add-column-impl *database-type* conn args))
 
-(defgeneric apply-create-migration-table-impl (database-type connection)
-  (:documentation "Implementation of create migration table"))
+(defun add-index (conn &rest args &key &allow-other-keys)
+  "Run add index query according to the database implementation"
+  (apply #'add-index-impl *database-type* conn args))
 
+(defun drop-table (conn &rest args &key &allow-other-keys)
+  "Run drop table query according to the database implementation"
+  (apply #'drop-table-impl *database-type* conn args))
 
-;; db/migrate
-;; - find . -type f -name '*.lisp' | sort
-;; - already patched?
-;;   - generate sql
-;;   - apply sql
-;;   - insert migration table
-(defun %db/migrate (basedir)
-  (with-db-connection-direct (connection)
-    (let ((files (directory (format NIL "~A/db/migrate/**/*.lisp" basedir))))
-      (dolist (file files)
-        (let ((filename (file-namestring file))
-              (migration-name (pathname-name file)))
-          ;; TODO: log
-          (format T "cheking migration: ~A~%" migration-name)
-          (when (not-migrated-p connection migration-name)
-            ;; TODO: log
-            (format T "  ... need migration: ~A~%" migration-name)
-            (with-open-file (in file)
-              (loop for sexp = (read in nil nil)
-                    while sexp
-                    do (let ((sql (parse-migration sexp)))
-                         (apply-migration connection sql migration-name))))))))))
+(defun drop-column (conn &rest args &key &allow-other-keys)
+  "Run drop column query according to the database implementation"
+  (apply #'drop-column-impl *database-type* conn args))
 
+(defun drop-index (conn &rest args &key &allow-other-keys)
+  "Run drop index query according to the database implementation"
+  (apply #'drop-index-impl *database-type* conn args))
 
-(defun apply-sql (connection sql)
-  (dbi:do-sql connection sql))
+(defun db-create ()
+  "implementation of `db/create`. create database and migration table"
+  (ensure-database)
+  (ensure-migration-table))
 
-(defun not-migrated-p (connection migration-name)
-  (let* ((query (dbi:prepare connection "select * from migration where migration_name = ?"))
-         (result (dbi:execute query (list migration-name))))
-    (null (dbi:fetch result))))
+(defun db-migrate (basedir)
+  "implementation of `db/migrate`. load migration files and apply migrations"
+  (ensure-migration-table)
+  (load-migration-files basedir)
+  (migrate-all))
 
-(defun apply-migration (connection sql migration-name)
-  (dbi:do-sql connection sql)
-  (dbi:do-sql connection "insert into migration (migration_name) values (?)" (list migration-name)))
-
-
-(defun find-table (table)
-  (assoc table *tbl* :test #'string=))
-
-(defun parse-migration (s)
-  "Parses the S-expression for migration, adds its content to *tbl*, and returns the SQL."
-  (when (null s)
-    (error "empty"))
-  (when (atom s)
-    (error "atom:~A" s))
-  (let ((op (car s)))
-    (cond ((string= "CREATE-TABLE" (symbol-name op))
-           ;; (create-table "todo"
-           ;;   (("title" :type :string)
-           ;;    ("done" :type :boolean)))
-           (let ((table-name (cadr s))
-                 (body (caddr s)))
-              (update-tbl-create-table table-name body)
-              (generate-create-table table-name body)))
-          ((string= "ADD-COLUMN" (symbol-name op))
-           ;; (add-column "todo"
-           ;;   (("done-at" :type :datetime)))
-           (let ((table-name (cadr s))
-                 (body (caddr s)))
-              (update-tbl-add-column table-name body)
-              (generate-add-column table-name body)))
-          (t (error "unknown operation: ~A" op)))))
-
-(defun generate-create-table (table body)
-  (let* ((base '(("id" :type :integer
-                      :not-null T
-                      :primary-key T
-                      :auto-increment T)
-                  ("created-at" :type :datetime
-                              :not-null T)
-                  ("updated-at" :type :datetime
-                              :not-null T)))
-         (body (append base body)))
-    (format NIL "create table ~A ( ~{ ~A~^, ~} )"
-            table
-            (parse-create-table body))))
-
-(defun generate-add-column (table columns)
-  (format nil "alter table ~A ~A" table (parse-add-column columns)))
-
-(defun update-tbl-create-table (table columns)
-  (when (find-table table)
-    (error "~A already defined" table))
-  (setq *tbl* (push (cons table columns) *tbl*)))
-
-(defun update-tbl-add-column (table columns)
-  (when (null (find-table table))
-    (error "~A not defined yet" table))
-  (let ((table-columns (cdr (find-table table))))
-     (nconc table-columns columns)))
-
-(defun parse-create-table (body)
-  (when (null body)
-    (error "emtpy body"))
-  (loop for col in body
-        collect (parse-column col)))
-
-(defun parse-add-column (body)
-  (when (null body)
-    (error "emtpy body"))
-  (parse-add-column-impl *database-type* (loop for col in body
-                                               collect (parse-column col))))
-  
-(defun parse-column (col)
-  (when (null col)
-    (error "empty column"))
-  (let* ((column_name (kebab->snake (car col)))
-          (attr (cdr col))
-          (type (getf attr :type))
-          (not-null-p (getf attr :not-null))
-          (primary-key-p (getf attr :primary-key))
-          (auto-increment-p (getf attr :auto-increment)))
-    (check-type-valid type)
-    (create-column column_name type not-null-p primary-key-p auto-increment-p)))
-  
 (defun check-type-valid (type)
   (when (not (find type *type-list*))
     (error "type error: ~A" type)))
 
-(defun create-column (column_name type not-null-p primary-key-p auto-increment-p)
-  (create-column-impl *database-type* :column-name column_name
-                                      :type type
-                                      :not-null-p not-null-p
-                                      :primary-key-p primary-key-p
-                                      :auto-increment-p auto-increment-p))
 
-(defgeneric parse-add-column-impl (database-type columns)
-  (:documentation "Implementation of parse add column"))
+;;;;
+;;;; internal functions
+;;;;
 
+(defun ensure-database ()
+  (with-db-connection-direct (connection :no-database T)
+    (ensure-database-impl *database-type* connection)))
+
+(defun ensure-migration-table ()
+  (with-db-connection-direct (connection)
+    (ensure-migration-table-impl *database-type* connection)))
+
+(defun load-migration-files (basedir)
+  (let ((files (directory (format NIL "~A/db/migrate/**/*.lisp" basedir))))
+    (dolist (file files)
+      (format T "loading migration file: ~A" file)
+      (load file)
+      (format T " ... done~%"))))
+
+(defun migrate-all ()
+  (with-db-connection-direct (connection)
+    (loop for migration in *migrations*
+        when (not-migrated-p connection (getf migration :migration-name))
+        do (apply-migration connection migration))))
+
+(defun apply-migration (connection migration)
+  (let* ((migration-name (getf migration :migration-name))
+         (up-fn (getf migration :up)))
+    (funcall up-fn connection)
+    (insert-migration connection migration-name)))
+
+
+(defun not-migrated-p (connection migration-name)
+  (format T "checking migration: ~A~%" migration-name)
+  (let* ((query (dbi:prepare connection "select * from migration where migration_name = ?"))
+         (result (dbi:execute query (list migration-name))))
+    (null (dbi:fetch result))))
+
+(defun insert-migration (connection migration-name)
+  (dbi:do-sql connection "insert into migration (migration_name) values (?)" (list migration-name)))
+
+ 
