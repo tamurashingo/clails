8e8dbc3016e612ece936601db67486cebb9b9400
tamura shingo <tamura.shingo@gmail.com>
2024-11-21 20:17:44 +0900

    add base-model

--- src/model/base-model.lisp
+++ src/model/base-model.lisp
@@ -5,12 +5,51 @@
 (in-package #:clails/model/base-model)
 
 (defclass <base-model> ()
-  ((id
-     :initarg :id
-     :accessor id)
-   (created-at
-     :initarg :created-at
-     :accessor created-at)
-   (updated-at
-     :initarg :updated-at
-     :accessor updated-at)))
+  ((data :initform (make-hash-table))
+   (save-p :initform nil)))
+
+(defmethod initialize-instance :after ((m <base-model>) &rest initargs)
+  (declare (ignorable initargs))
+  ;; make base field
+  (setf (gethash :id (slot-value m 'data)) nil)
+  (setf (gethash :created-at (slot-value m 'data)) nil)
+  (setf (gethash :updated-at (slot-value m 'data)) nil))
+
+(defmethod ref ((inst <base-model>) key)
+  (multiple-value-bind (value present-p)
+      (gethash key (slot-value inst 'data))
+    (when (not present-p)
+      (error "not found slot name: ~A" key))
+    value))
+
+(defun (setf ref) (new-value inst key)
+  (multiple-value-bind (value present-p)
+      (gethash key (slot-value inst 'data))
+    (declare (ignorable value))
+    (when (not present-p)
+      (error "not found slot name: ~A" key)))
+  (setf (gethash key (slot-value inst 'data))
+        new-value))
+
+(defun show-model (model)
+  (maphash #'(lambda (key value)
+               (format t "~A:~A~%" key value))
+           (slot-value model 'data)))
+
+
+;;
+;; (defclass <todo> (<base-model>) ())
+;; (defmethod initialize-instance :after ((m <todo>) &rest initargs)
+;;   ;; make field
+;;   (setf (gethash :title (slot-value m 'data)) nil)
+;;   (setf (gethash :done (slot-value m 'data)) nil))
+;; (defvar todo (make-instance '<todo>))
+;;
+;; (setf (ref todo :title) "refactor all products")
+;; (show-model todo)
+;;
+;; ID:NIL
+;; CREATED-AT:NIL
+;; UPDATED-AT:NIL
+;; TITLE:refactor all products
+;; DONE:NIL
