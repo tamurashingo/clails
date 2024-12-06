1a021b203af92d16c43092c87b571ded19b28d4b
tamura shingo <tamura.shingo@gmail.com>
2024-12-07 06:43:26 +0900

    add export

--- src/cmd.lisp
+++ src/cmd.lisp
@@ -14,7 +14,9 @@
                 #:db-migrate)
   (:export #:create-project
            #:generate/model
-           #:generate/migration))
+           #:generate/migration)
+           #:db/create
+           #:db/migrate)
 (in-package #:clails/cmd)
 
 (defun create-project (project-name &key project-path (database :sqlite3))
