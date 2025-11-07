a7df0d173ccc25412f29679cb4ab6f99bd7c8458
tamura shingo <tamura.shingo@gmail.com>
2025-11-07 04:28:03 +0000

    add e2e debug message

--- test/e2e/todo-app-e2e.sh
+++ test/e2e/todo-app-e2e.sh
@@ -145,6 +145,13 @@ clails stop
 
 echo ""
 echo "=== Preparing test database ==="
+
+echo "-- test-loader"
+cats test/test-loader.lisp
+echo "-- done"
+echo ""
+
+
 echo "Creating test database..."
 CLAILS_ENV=test clails db:create
 
