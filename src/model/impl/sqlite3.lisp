3dfabf2828770026eff090b0b4eeb54e23a33fb8
tamura shingo <tamura.shingo@gmail.com>
2025-11-07 04:06:32 +0000

    Fix SQLite3 datetime conversion to support local-time:timestamp

--- src/model/impl/sqlite3.lisp
+++ src/model/impl/sqlite3.lisp
@@ -72,9 +72,13 @@
                                      (parse-integer (subseq v 0 4)))))
                    :cl-db-fn ,#'(lambda (ut)
                                   (when ut
-                                    (multiple-value-bind (sec min hour date month year day daylight-p zone)
-                                        (decode-universal-time ut)
-                                      (format nil "~4,\'0d-~2,\'0d-~2,\'0d ~2,\'0d:~2,\'0d:~2,\'0d" year month date hour min sec))))))
+                                    (let ((universal-time 
+                                           (if (typep ut 'local-time:timestamp)
+                                               (local-time:timestamp-to-universal ut)
+                                               ut)))
+                                      (multiple-value-bind (sec min hour date month year day daylight-p zone)
+                                          (decode-universal-time universal-time)
+                                        (format nil "~4,\'0d-~2,\'0d-~2,\'0d ~2,\'0d:~2,\'0d:~2,\'0d" year month date hour min sec)))))))
     ("date" . (:type :date
                :db-cl-fn ,#'(lambda (v)
                               (when v
